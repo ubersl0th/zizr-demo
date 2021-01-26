2967d2df50c9d9d914ec629203c16d6341942f5d
Eivind Furuberg <eivind.furuberg@gmail.com>
2021-01-26 13:37:16 +0100

    fix whitespace

--- index.js
+++ index.js
@@ -23,37 +23,38 @@ const neo4j = require("neo4j-driver");
     // create contraints
     await session.writeTransaction((tx) => {
       tx.run(
-        "CREATE CONSTRAINT IF NOT EXISTS ON (u:User) ASSERT u.id IS UNIQUE"
+        "CREATE CONSTRAINT IF NOT EXISTS ON (u:User) ASSERT u.userId IS UNIQUE"
       );
       tx.run(
-        "CREATE CONSTRAINT IF NOT EXISTS ON (p:Product) ASSERT p.id IS UNIQUE"
+        "CREATE CONSTRAINT IF NOT EXISTS ON (p:Product) ASSERT p.productId IS UNIQUE"
       );
     });
 
     // create som users
     await session.writeTransaction((tx) => {
-      tx.run(`CREATE 	(:User {id: "a4dz"}),
-                      (:User {id: "f72m"}),
-                      (:User {id: "o4d1"}),
-                      (:User {id: "gz3c"}),
-                      (:User {id: "b32w"})`);
+      tx.run(`CREATE 	(:User {userId: "a4dz"}),
+                      (:User {userId: "f72m"}),
+                      (:User {userId: "o4d1"}),
+                      (:User {userId: "gz3c"}),
+                      (:User {userId: "b32w"})`);
     });
 
     // create products from csv
-    await session.writeTransaction((tx) => {
+    await session.writeTransaction((tx) =>
       tx.run(
         `LOAD CSV FROM 'https://raw.githubusercontent.com/ubersl0th/zizr-demo/main/products.csv' AS line
-        CREATE (:Product { id: line[0], model: line[1], category: line[2], size: line[3]})`
-      );
-    });
+        CREATE (:Product { productId: line[0], model: line[1], category: line[2], size: line[3]})`
+      )
+    );
 
     // create relations from csv
-    await session.writeTransaction((tx) => {
+    var result = await session.writeTransaction((tx) =>
       tx.run(
         `LOAD CSV FROM 'https://raw.githubusercontent.com/ubersl0th/zizr-demo/main/relations.csv' AS line
-        MATCH (u:User),(p:Product) WHERE u.id = line[0] AND p.id = line[1] MERGE (u)-[:OWNS]->(p)`
-      );
-    });
+        MATCH (u:User),(p:Product) WHERE u.userId = line[0] AND p.productId = line[1] CREATE (u)-[:OWNS]->(p)`
+      )
+    );
+    console.log(result.summary);
   } catch (e) {
     console.log(e);
   } finally {
